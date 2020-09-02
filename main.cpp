f1e0870a76d3d1a9f6cc7adc992d814e77bca520
xiaokaisheng <980872878@qq.com>
2020-09-02 10:26:20 +0800

    add Qt Demo Project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QProcess>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    print("hello world");
+    w.show();
+
+    return a.exec();
+}
