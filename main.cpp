624a15d571a97ded93bd4ee66b1b2c163c4cace2
xiaokaisheng <980872878@qq.com>
2020-09-05 11:25:49 +0800

    Resolve compilation errors

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
-﻿#include "mainwindow.h"
+#include "mainwindow.h"
 #include <QApplication>
-#include <QProcess>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    print("hello world");
     w.show();
-
     return a.exec();
 }
